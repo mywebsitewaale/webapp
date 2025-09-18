8dc43cdfbaa0b064b8b2228f9a609bf827621208
hintonbrothersprivatelimited <hintonbrothersprivatelimited@gmail.com>
2025-09-18 20:25:25 +0530

    Testing Project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import logo from './assets/logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://websitewaale.com" target="_blank">
+          <img src={logo} className="logo react" alt="website.waale" />
+        </a>
+          <h1>WebsiteWaale Solutions</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
