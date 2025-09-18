23c66ec5612ba6750c7f41d1d248661d6ec96442
hintonbrothersprivatelimited <hintonbrothersprivatelimited@gmail.com>
2025-09-18 22:37:17 +0530

    Testing Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import logo from './assets/logo.png'
 import './App.css'
 
-function App() {
-  return (
-    <>
-      <div>
-        <a href="https://websitewaale.com" target="_blank">
-          <img src={logo} className="logo react" alt="website.waale" />
-        </a>
-          <h1>WebsiteWaale Solutions</h1>
-      </div>
-    </>
-  )
+export default function App() {
+    return ( // WebApp
+        <nav>
+            <div>
+                <a href="https://websitewaale.com" target="_blank">
+                    <img src={logo} className="logo react" alt="website.waale"/>
+                </a>
+                <h1>WebsiteWaale Solutions</h1>
+            </div>
+        </nav>
+    )
 }
-
-export default App
